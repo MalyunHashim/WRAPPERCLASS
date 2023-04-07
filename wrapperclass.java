d2ae89968aa116660ed6e1ec025da408f8fe491b
malyumohamed <malyunmohaz942@gmail.com>
2023-04-08 02:04:13 +0300

    main

---
+++ wrapperclass.java
@@ -0,0 +1,12 @@
+package WRAPPERCLASS;
+
+public class wrapperclass {
+    public static void main(String[] args) {
+        Integer myInt=10;
+        Double myDouble=2.55;
+        Character myChar='F';
+        System.out.println(myInt.intValue());
+        System.out.println(myDouble.doubleValue());
+        System.out.println(myChar.charValue());
+    }
+}
